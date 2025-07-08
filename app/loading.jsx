999c50110d5167a3fa81c5c2b5f04c2e4c0ead70
pivnati <natanmuleta911@gmail.com>
2025-07-07 18:44:46 -0700

    modfiy the loading page

--- app/loading.jsx
+++ app/loading.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
-
-const Loading = () => {
-  return (
-    <div>Loading</div>
-  )
-}
-
-export default Loading
+export default function Loading() {
+    return (
+      <main className="text-center">  
+        <h2 className="text-primary">Loading...</h2>
+        <p>Hopefully not for to long :)</p>
+      </main>
+    )
+  }
